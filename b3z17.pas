b3683044de6ba3c4b50f532684863b6ca2624f49
gnaumkin <144417191+gnaumkin@users.noreply.github.com>
2023-09-19 14:14:22 +0300

    b3z17.pas

--- b3z17.pas
+++ b3z17.pas
@@ -1,8 +1,11 @@
 ﻿program b3z17;
-var a,b,c,d:integer;
+var a,b,c,d,x1,x2:real;
 begin
   writeln ('Напишите коэффициенты a,b,c');
   read (a,b,c);
   d:=(b*b)-4*a*c;
-  writeln('Корень квадратного уравнения равен = ', d);
+  x1:=(-b - sqrt(d))/(2*a);
+  x2:=(-b + sqrt(d))/(2*a);
+  writeln('1 корень квадратного уравнения равен = ', x1);
+  writeln('2 корень квадратного уравнения равен = ', x2);
 end.
