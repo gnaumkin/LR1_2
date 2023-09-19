2e83cc0761f3f67682762535e7cfb98014c407d0
gnaumkin <144417191+gnaumkin@users.noreply.github.com>
2023-09-19 10:41:05 +0300

    Add files via upload

---
+++ b4z26.pas
@@ -0,0 +1,8 @@
+﻿program b4z26;
+var a,b,c:real;
+begin
+writeln ('Введите 3 стороны треугольника');
+readln (a,b,c);
+if (a + b > c) and (b + c > a) and (a + c > b) then writeln ('Треугольник с такими сторонами существует') 
+else writeln ('Треугольника с такими сторонами не существует')
+end.
---
+++ b4z28.pas
@@ -0,0 +1,10 @@
+﻿Program b4z28; 
+var n,k:integer; 
+Begin writeln ('Введите число n');
+readln (n); 
+k:=n-(n div 10)*10; 
+write ('На лугу пасется ',n,' '); 
+if ((k=0) or (k=5) or (k=6) or (k=7) or (k=8) or (k=9)) or ((n>=10) and (n<20)) then writeln ('коров'); 
+if (k=1) and not ((n>=10) and (n<20)) then writeln ('корова'); 
+if ((k=2) or (k=3) or (k=4)) and not ((n>=10) and (n<20)) then writeln ('коровы'); 
+end.
