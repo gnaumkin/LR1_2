30fb871bb813119c89617ab1d05d77389350e2e0
gnaumkin <144417191+gnaumkin@users.noreply.github.com>
2023-09-19 14:16:25 +0300

    b3z18.pas

--- b3z18.pas
+++ b3z18.pas
@@ -4,6 +4,6 @@ begin
   writeln ('Напишите 1 число');
   read (a);
   if( a mod 4 = 0) and (a mod 100<>0) or (a mod 400 = 0) then
-   writeln('YES') else
-   writeln('NO');
+   writeln('Год високосный') else
+   writeln('Год не високосный');
 end.
