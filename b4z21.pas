892a061dceca38877e39abb640295efbdc4c0e50
gnaumkin <144417191+gnaumkin@users.noreply.github.com>
2023-09-19 12:49:26 +0300

    b4z21.pas

--- b4z21.pas
+++ b4z21.pas
@@ -1,7 +1,7 @@
 ﻿program b4z21;
 var a,b,c:integer;
 begin
-writeln ('Введите число');
+writeln ('Введите числа');
 readln (a,b,c);
 if ((a mod 2=0) or (b mod 2=0) or (c mod 2=0)) and ((a mod 2=1) or (b mod 2=1) or (c mod 2=1)) then
 writeln ('Есть хотя бы одно четное и одно нечетное число') else writeln ('Нет хотя бы одного четного и одного нечетного числа');
